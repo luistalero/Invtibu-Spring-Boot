c532f3c671d5934ebc1cfc5c64ee0c2e1df7a917
luistalero <luisalbertotaleromartinez@gmail.com>
2025-04-28 15:50:54 -0500

    feat: :tada: Spring Boot project initialization project to generate Rest Api's with connection to postgreSQL database is started.

---
+++ src/main/java/com/invetario/inventariotibu/InventariotibuApplication.java
@@ -0,0 +1,13 @@
+package com.invetario.inventariotibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariotibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariotibuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invetario/inventariotibu/InventariotibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invetario.inventariotibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariotibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
